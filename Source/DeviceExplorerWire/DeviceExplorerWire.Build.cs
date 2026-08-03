1d60d70cdea745f179df1ac8f939a60f7b457efc
microneo <edicmor@yandex.ru>
2026-08-03 23:31:47 +0300

    Build DeviceExplorerWire with C++20 and no PCH

--- Source/DeviceExplorerWire/DeviceExplorerWire.Build.cs
+++ Source/DeviceExplorerWire/DeviceExplorerWire.Build.cs
@@ -4,11 +4,11 @@ public class DeviceExplorerWire : ModuleRules
 {
     public DeviceExplorerWire(ReadOnlyTargetRules Target) : base(Target)
     {
-        PCHUsage = PCHUsageMode.NoSharedPCHs;
+        PCHUsage = PCHUsageMode.NoPCHs;
         bRequiresImplementModule = false;
         bWarningsAsErrors = true;
         bEnableExceptions = false;
         bUseRTTI = false;
-        CppStandard = CppStandardVersion.Cpp17;
+        CppStandard = CppStandardVersion.Cpp20;
     }
 }
